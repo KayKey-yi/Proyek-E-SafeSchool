f10cbc2447292a21dc9aa3d36a14ee693ad4953d
Kayana <nkayana.rtndht16@gmail.com>
2026-09-22 10:31:55 +0700

    role guru permanent

--- database/seeders/RoleTableSeeder.php
+++ database/seeders/RoleTableSeeder.php
@@ -18,6 +18,7 @@ class RoleTableSeeder extends Seeder
         foreach ([
             ['role' => 'Super Admin', 'level' => 1],
             ['role' => 'Admin', 'level' => 2],
+            ['role' => 'Guru', 'level' => 2],
             ['role' => 'Siswa', 'level' => 3],
         ] as $role) {
             Role::updateOrCreate(['role' => $role['role']], $role);
